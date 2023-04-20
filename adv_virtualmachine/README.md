f96f2e9a873cf560ef53c20a697db8e03a7c8174
barbara <zhujianwang001@gmail.com>
2023-04-20 13:50:05 +0800

    添加virtual machine模块解读

---
+++ adv_virtualmachine/README.md
@@ -0,0 +1,8 @@
+# `virtual machine`模块解读
+
+
+
+
+
+## 参考链接
+* 1 [`pytorch`官方发布`resnet50`模型预处理](https://pytorch.org/vision/master/models/generated/torchvision.models.resnet50.html)
--- adv_virtualmachine/demo.py
+++ adv_virtualmachine/demo.py
@@ -1,13 +1,14 @@
 import torch
 import numpy as np
 import torchvision.models as models
+import cv2
 
 import tvm
 from tvm import relay
 from tvm.runtime.vm import VirtualMachine
 
 import sys
-sys.path.append("..")
+sys.path.append("/share4algo02/e00130/workspace/tutorial/TensorCompiler/TVM/20230307/tvm/tvm_walk_through")
 from utils import array_des
 from visualize import RelayVisualizer
 
@@ -21,9 +22,19 @@ def check_optimize(mod,target,params):
 
 if __name__=='__main__':
   #prepare model and input
-  model = models.resnet18(pretrained=True)
+  model = models.resnet18(pretrained=True, progress=True)
   shape_list = [("input0",(1,3,224,224))]
-  fake_input = np.random.random_sample(shape_list[0][1]).astype('float32')
+  # fake_input = np.random.random_sample(shape_list[0][1]).astype('float32')
+  img_path = "/share4algo02/e00130/workspace/tutorial/TensorCompiler/TVM/20230307/tvm/cat.png"
+  img = cv2.imread(img_path).astype("float32")
+  img = cv2.resize(img, tuple(shape_list[0][1][2:4]))
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  # img = np.transpose(img / 255.0, [2, 0, 1])
+  img /= 255.0
+  img -= np.array([0.485, 0.456, 0.406])
+  img /= np.array([0.229, 0.224, 0.225])
+  img = np.transpose(img, [2, 0, 1])
+  fake_input = np.expand_dims(img, axis=0)
   graph = torch.jit.trace(model,torch.from_numpy(fake_input))
 
   #step 1 parse to relay
